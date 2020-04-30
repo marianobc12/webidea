153064a84b94ec9a797b0f119f2aec1312d3f671
marianobc12 <marianobc12@hotmail.com>
2020-04-30 02:51:55 -0300

    menu en otro archivo

--- contacto.php
+++ contacto.php
@@ -26,27 +26,9 @@
     </script>
 </head>
 <body>
-    <nav class="navbar  navbar-expand-lg navbar-light">
-        <a class="navbar-brand" href="#">
-          <img class="logo" src="img/logo.png" width="150" height="40" class="d-inline-block align-top" alt="WEBIDEA">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="index"><i class="fas fa-home fa-lg"></i> Inicio</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="servicios"><i class="fas fa-laptop fa-lg"></i> Servicios</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="contacto"><i class="fab fa-telegram-plane fa-lg"></i> Contacto</a>
-              </li>
-            </ul>
-        </div>
-    </nav>
+    <?php
+      include 'menu.php';
+    ?>
     <div class="container contenedor-contacto">
       <div class="row">
         <div class="col-12 animated fadeIn slower">
